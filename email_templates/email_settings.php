33dd00d670d991f61ddd515ce31dc0cdd273eec7
AsifAhmedAli <asif@mexil.it>
2022-06-29 16:40:04 +0500

    local server

--- email_templates/email_settings.php
+++ email_templates/email_settings.php
@@ -16,7 +16,7 @@ $mail = new PHPMailer(true);
     $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'asif@mexil.it';                     //SMTP username
-    $mail->Password   = 'Jaan3038611@';                               //SMTP password
+    $mail->Password   = 'Jaan3038611';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 ?>

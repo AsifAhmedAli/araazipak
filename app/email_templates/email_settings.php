6f358f5c5e024379c93225ca23865ddeb033ccd6
AsifAhmedAli <aaali.bscs15seecs@seecs.edu.pk>
2024-08-05 01:17:28 +0500

    Update email_settings.php

--- app/email_templates/email_settings.php
+++ app/email_templates/email_settings.php
@@ -14,9 +14,7 @@ $mail = new PHPMailer(true);
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'info@mexil.it';                     //SMTP username
-    $mail->Password   = 'Jaan123456';                               //SMTP password
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication                         //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 ?>
